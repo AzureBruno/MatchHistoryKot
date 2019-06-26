16a457b8d304e05593891618ab675bc0a380d5a7
Yuri Bunduki <yuribunduki@gmail.com>
2019-06-25 22:24:00 -0300

    ainda problemas no loader

--- app/src/main/java/com/example/picassoexemplo/HistoryActivity.kt
+++ app/src/main/java/com/example/picassoexemplo/HistoryActivity.kt
@@ -1,13 +1,17 @@
 package com.example.picassoexemplo
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.View
+import android.widget.ListView
 import com.github.kittinunf.fuel.httpGet
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_history.*
+import kotlinx.android.synthetic.main.activity_list_item.*
 import org.json.JSONObject
 
 class HistoryActivity  : AppCompatActivity() {
@@ -16,11 +20,14 @@ class HistoryActivity  : AppCompatActivity() {
 
     var Matches = ArrayList<Match>()
 
+    private lateinit var listView : ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var URL = baseURL + intent.getStringExtra("SUMMONER") // erro de putExtra
         setContentView(R.layout.activity_history)
         val recyclerView = match_history
+        listView = findViewById<ListView>(R.id.match_history)
 
         //URL.httpGet().responseObject(Match.Deserializer()) { request, response, result ->
         URL.httpGet().responseString() { request, response, result ->
@@ -40,13 +47,28 @@ class HistoryActivity  : AppCompatActivity() {
             while ( i < match.length()){
                 println("teste3"+match.getJSONObject(i))
                 Matches.add(Fatiar(match.getJSONObject(i)))
+                println("teste2"+Matches.size)
                 i++
             }
-
         }
-        recyclerView.adapter = MatchListAdapter(Matches, this)
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+//        recyclerView.adapter = MatchListAdapter(Matches, this)
+//        val layoutManager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = layoutManager
 
+        val adapter = MatchListAdapter(Matches, this)
+        println("teste4")
+        listView.adapter = adapter
+//        while (Matches.size < 10){
+//            refreshView()
+//        }
+//        when(Matches.size > 2) {
+//            refreshView()
+//        }
+
+    }
+
+    private fun refreshView(){
+        findViewById<ConstraintLayout>(R.id.list_item).visibility = View.INVISIBLE
+        findViewById<ConstraintLayout>(R.id.list_item).visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/example/picassoexemplo/MatchListAdapter.kt
+++ app/src/main/java/com/example/picassoexemplo/MatchListAdapter.kt
@@ -6,75 +6,133 @@ import android.support.v7.widget.RecyclerView.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_list_item.view.*
 
-class MatchListAdapter(private val matches: List<Match>, private val context: Context) : Adapter<MatchListAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val view = LayoutInflater.from(p0.context).inflate(R.layout.activity_list_item, p0, false)!!
-        return ViewHolder(view)
-    }
+class MatchListAdapter(private val matches: List<Match>, private val context: Context) : BaseAdapter() {
 
-    override fun getItemCount(): Int {
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    //1
+    override fun getCount(): Int {
         return matches.size
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        val match = matches[p1]
-        p0?.let {
-            it.bindView(match)
-        }
+    //2
+    override fun getItem(position: Int): Any {
+        return matches[position]
+    }
+
+    //3
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    //4
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // Get view for row item
+        val rowView = inflater.inflate(R.layout.activity_list_item, parent, false)
+
+        val imageChampion = rowView.findViewById(R.id.image_champion) as ImageView
+        val textQueue = rowView.findViewById(R.id.text_queue) as TextView
+        val textScore = rowView.findViewById(R.id.text_score) as TextView
+        val imgItem0 = rowView.findViewById(R.id.img_item0) as ImageView
+        val imgItem1 = rowView.findViewById(R.id.img_item1) as ImageView
+        val imgItem2 = rowView.findViewById(R.id.img_item2) as ImageView
+        val imgItem3 = rowView.findViewById(R.id.img_item3) as ImageView
+        val imgItem4 = rowView.findViewById(R.id.img_item4) as ImageView
+        val imgItem5 = rowView.findViewById(R.id.img_item5) as ImageView
+
+
+        val match = getItem(position) as Match
+
+        Picasso.get().load(match.championPNG).placeholder(R.mipmap.ic_launcher).into(imageChampion)
+        textQueue.text = match.win
+        textScore.text = "${match.kills}/${match.deaths}/${match.assists}"
+        Picasso.get().load(match.item0URL).placeholder(R.mipmap.ic_launcher).into(imgItem0)
+        Picasso.get().load(match.item1URL).placeholder(R.mipmap.ic_launcher).into(imgItem1)
+        Picasso.get().load(match.item2URL).placeholder(R.mipmap.ic_launcher).into(imgItem2)
+        Picasso.get().load(match.item3URL).placeholder(R.mipmap.ic_launcher).into(imgItem3)
+        Picasso.get().load(match.item4URL).placeholder(R.mipmap.ic_launcher).into(imgItem4)
+        Picasso.get().load(match.item5URL).placeholder(R.mipmap.ic_launcher).into(imgItem5)
+
+        return rowView
     }
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bindView(match: Match) {
 
-            val imageChampion = itemView.image_champion
-            val textQueue = itemView.text_queue
-            val textScore = itemView.text_score
-            val imgItem0 = itemView.img_item0
-            val imgItem1 = itemView.img_item1
-            val imgItem2 = itemView.img_item2
-            val imgItem3 = itemView.img_item3
-            val imgItem4 = itemView.img_item4
-            val imgItem5 = itemView.img_item5
 
-            Picasso
-                .get()
-                .load(match.championPNG)
-                .into(imageChampion)
-            textQueue.text = match.win
-            textScore.text = "${match.kills}/${match.deaths}/${match.assists}"
-            /*
-                Load item images
-                 */
-            Picasso
-                .get()
-                .load(match.item0URL)
-                .into(imgItem0)
-            Picasso
-                .get()
-                .load(match.item1URL)
-                .into(imgItem1)
-            Picasso
-                .get()
-                .load(match.item2URL)
-                .into(imgItem2)
-            Picasso
-                .get()
-                .load(match.item3URL)
-                .into(imgItem3)
-            Picasso
-                .get()
-                .load(match.item4URL)
-                .into(imgItem4)
-            Picasso
-                .get()
-                .load(match.item5URL)
-                .into(imgItem5)
-        }
 
-    }
+//    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+//        val view = LayoutInflater.from(p0.context).inflate(R.layout.activity_list_item, p0, false)!!
+//        return ViewHolder(view)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return matches.size
+//    }
+//
+//    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+//        val match = matches[p1]
+//        p0?.let {
+//            it.bindView(match)
+//        }
+//    }
+
+
+//    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//
+//        fun bindView(match: Match) {
+//
+//            val imageChampion = itemView.image_champion
+//            val textQueue = itemView.text_queue
+//            val textScore = itemView.text_score
+//            val imgItem0 = itemView.img_item0
+//            val imgItem1 = itemView.img_item1
+//            val imgItem2 = itemView.img_item2
+//            val imgItem3 = itemView.img_item3
+//            val imgItem4 = itemView.img_item4
+//            val imgItem5 = itemView.img_item5
+//
+//            Picasso
+//                .get()
+//                .load(match.championPNG)
+//                .into(imageChampion)
+//            textQueue.text = match.win
+//            textScore.text = "${match.kills}/${match.deaths}/${match.assists}"
+//            /*
+//                Load item images
+//                 */
+//            Picasso
+//                .get()
+//                .load(match.item0URL)
+//                .into(imgItem0)
+//            Picasso
+//                .get()
+//                .load(match.item1URL)
+//                .into(imgItem1)
+//            Picasso
+//                .get()
+//                .load(match.item2URL)
+//                .into(imgItem2)
+//            Picasso
+//                .get()
+//                .load(match.item3URL)
+//                .into(imgItem3)
+//            Picasso
+//                .get()
+//                .load(match.item4URL)
+//                .into(imgItem4)
+//            Picasso
+//                .get()
+//                .load(match.item5URL)
+//                .into(imgItem5)
+//        }
+//
+//    }
 }
